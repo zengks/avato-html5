831ec803ee988c420ec0e1a94932fe4b1d96dda5
Chen Zeng <info@czeng.ca>
2022-12-19 13:01:03 -0800

    bind the methods from the class

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Form submit using onSubmit method
 // Toolbar button click using onToolbarClick method
 
-const body = document.querySelector('body')
-const menuBtn = document.getElementById('hamburger')
+const body = document.querySelector('body');
+const menuBtn = document.getElementById('hamburger');
 const contactForm = document.getElementById('contactForm');
-const btn1 = document.getElementById('btn1')
-const btn2 = document.getElementById('btn2')
-const btn3 = document.getElementById('btn3')
-const btn4 = document.getElementById('btn4')
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
 
 menuBtn.addEventListener('click', () => {
-    body.classList.toggle('is-active')
+    body.classList.toggle('is-active');
 })
 
 class contact {
@@ -19,41 +19,46 @@ class contact {
         this.email = email;
         this.subject = subject;
         this.message = message;
+        this.onSubmit = this.onSubmit.bind(this);
+        this.btnClick = this.btnClick.bind(this);
     }
 
-    onSubmit(name, email, subject, message) {
-        let result = `Thank you for contact us. The following message has been sent successfully.\n\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+    onSubmit() {
+        let result = `Thank you for contact us. The following message has been sent successfully.\n\nName: ${this.name}\nEmail: ${this.email}\nSubject: ${this.subject}\nMessage: ${this.message}`;
         alert(result);
     }
 
-    btnClick(name, email, subject, message) {
-        let result = `You've clicked this button. Here's the message:\n\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+    btnClick() {
+        let result = `You've clicked this button. Here's the message:\n\nName: ${this.name}\nEmail: ${this.email}\nSubject: ${this.subject}\nMessage: ${this.message}`;
         alert(result);
     }
 }
-
-let form = new contact();
 
 contactForm.addEventListener('submit', (e) => {
     const name = e.target.name.value;
     const email = e.target.email.value;
     const subject = e.target.subject.value;
     const message = e.target.message.value;
-    form.onSubmit(name, email, subject, message);
+    let form = new contact(name, email, subject, message);
+    form.onSubmit();
 })
 
-btn1.addEventListener('click', (e) => {
-    form.btnClick('Steven', 'steven@email.com', 'Web Dev', 'Hello World');
+btn1.addEventListener('click', () => {
+    let form = new contact('Steven', 'steven@email.com', 'Web Dev', 'Hello World');
+    form.btnClick();
 })
 
-btn2.addEventListener('click', (e) => {
-    form.btnClick('Tracy', 'tracy@email.com', 'Bill Statement', 'Double check with last statement.');
+btn2.addEventListener('click', () => {
+    let form = new contact('Tracy', 'tracy@email.com', 'Bill Statement', 'Double check with last statement.');
+    form.btnClick();
 })
 
-btn3.addEventListener('click', (e) => {
-    form.btnClick('Santa', 'santa@email.com', 'Time of the Year', 'It is now XMAS');
+btn3.addEventListener('click', () => {
+    let form = new contact('Santa', 'santa@email.com', 'Time of the Year', 'It is now XMAS');
+    form.btnClick();
 })
 
-btn4.addEventListener('click', (e) => {
-    form.btnClick('Bill', 'bill@email.com', 'Software', 'Microsoft');
+btn4.addEventListener('click', () => {
+    let form = new contact('Bill', 'bill@email.com', 'Software', 'Microsoft');
+    form.btnClick();
 })
